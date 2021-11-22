76c31446b2469a90ed6377024b35e4075d89f2cb
Cliff Brake <cbrake@bec-systems.com>
2021-11-22 11:09:56 -0500

    add goreleaser stuff

---
+++ envsetup.sh
@@ -0,0 +1,16 @@
+# download goreleaser from https://github.com/goreleaser/goreleaser/releases/
+# and put in /usr/local/bin
+# This can be useful to test/debug the release process locally
+gitplm_goreleaser_build() {
+  goreleaser build --skip-validate --rm-dist
+}
+
+# before releasing, you need to tag the release
+# you need to provide GITHUB_TOKEN in env or ~/.config/goreleaser/github_token
+# generate tokens: https://github.com/settings/tokens/new
+# enable repo and workflow sections
+gitplm_goreleaser_release() {
+  #TODO add depend build to goreleaser config
+  siot_build_dependencies --optimize
+  goreleaser release
+}
